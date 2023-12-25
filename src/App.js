28df63c3c192a8a9897404ffd0ac37524aaf73ff
HieuLapTrinh04 <buihieu11012004@gmail.com>
2023-12-26 00:12:59 +0700

    login router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FormLogin from './components/FormLogin';
+import NoPage from "./components/NoPage";
 
 function App() {
   return (
     <div className='flex flex-col text-center bg-gradient-to-r from-purple-500 to-pink-500 w-full h-screen'>
-      <FormLogin />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FormLogin />}>
+          {/* <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} /> */}
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+      {/* <FormLogin /> */}
     </div>
   );
 }
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 export default App;
---
+++ src/components/NoPage.jsx
@@ -0,0 +1,6 @@
+const NoPage = () => {
+    return <div className="text-center bg-gradient-to-r from-orange-200 to-red-500 w-full h-screen">
+        <h1>Mời quay lại trang trước!!</h1>
+    </div>
+}
+export default NoPage
